15e6b871389a357bd9811c53f1b66f6defd25a35
Rishad Bharucha <rb2102@gmail.com>
2015-01-28 15:39:03 -0700

    Initial Commit

---
+++ MotionCapture/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MotionCapture
+//
+//  Created by Rishad Bharucha on 1/28/15.
+//  Copyright (c) 2015 Rishad Bharucha. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
